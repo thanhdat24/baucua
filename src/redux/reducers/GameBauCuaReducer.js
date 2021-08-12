3e8cb2da8c4f4c8e6b72ba05cacf785dba2d5247
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-08-12 12:02:56 +0700

    Update redux

---
+++ src/redux/reducers/GameBauCuaReducer.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+export const GameBauCua = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+};
+
+export default GameBauCua;
---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,6 @@
+import GameBauCua from "../../component/GameBauCua";
+import { combineReducers } from "redux";
+
+export const rootReducer = combineReducers({
+  GameBauCua,
+});
